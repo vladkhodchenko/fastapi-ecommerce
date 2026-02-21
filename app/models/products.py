1f4a93c64adacc5e77e88d135d899c3efdcd814b
hodchenko <sidyukvlad@gmail.com>
2026-02-21 14:22:20 +0300

    Add search index in model

--- app/models/products.py
+++ app/models/products.py
@@ -1,7 +1,8 @@
 from decimal import Decimal
 from datetime import datetime
 
-from sqlalchemy import String, Boolean, Integer, ForeignKey, text, Numeric, DateTime, func
+from sqlalchemy.dialects.postgresql import TSVECTOR
+from sqlalchemy import String, Boolean, Computed, Integer, ForeignKey, text, Numeric, DateTime, func, Index
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
@@ -24,7 +25,24 @@ class Product(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
 
+    tsv: Mapped[TSVECTOR] = mapped_column(
+        TSVECTOR,
+        Computed(
+            """
+            setweight(to_tsvector('english', coalesce(name, '')), 'A')
+            || 
+            setweight(to_tsvector('english', coalesce(description, '')), 'B')
+            """,
+            persisted=True,
+        ),
+        nullable=False,
+    )
+
 
     category: Mapped["Category"] = relationship("Category", back_populates="products")
     seller: Mapped["User"] = relationship("User", back_populates="products")
     reviews: Mapped["Review"] = relationship("Review", back_populates="product")
+
+    __table_args__ = (
+        Index("ix_products_tsv_gin", "tsv", postgresql_using="gin"),
+    )
